e416292ab8bf77fa03bf5e11d2579813585dcdcd
Ruben Barros <atosc08@gmail.com>
2020-10-15 11:19:19 +0200

    Css 2

--- index.php
+++ index.php
@@ -4,37 +4,31 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
   <title>Vakapp</title>
 </head>
 
 <body>
   <main>
-
     <section class="front">
       <img class="front_image" src="national-cancer-institute-opti.jpg" alt="Photo de médecins" />
       <h1 class="text_image">Rejoignez le mouvement Vakapp</h1>
     </section>
     <img class="logo" src="Logo_Vak.png" alt="Logo de VakApp" />
-
-    <section class="container first_container">
-
+    <section class="container">
       <article>
         <h2>L'application qui facilite les vacations</h2>
-        <p class="p_first_container">Découvrez Vakapp l'application qui à pour vocation de faciliter les vacations. Retrouvez des profils vérifiés, un agenda intégré, notre volonté : la simplicité.
+        <p>Découvrez Vakapp l'application qui à pour vocation de faciliter les vacations. Retrouvez des profils vérifiés, un agenda intégré, notre volonté : la simplicité.
       </article>
-
       <article>
         <h2>Qui sommes-nous ?</h2>
-        <p class="p_first_container">Nous sommes une jeune start-up qui avons des proches qui ont vécu ce problème et qui avons décidé d'y résoudre. Nous sommes 10 membres polyvalents et complémentaires qui regroupons nos compétences pour mettre au point l'application qui vise à simplifier et fiabiliser les vacations, dans les structures de la santé.</p>
+        <p>Nous sommes une jeune start-up qui avons des proches qui ont vécu ce problème et qui avons décidé d'y résoudre. Nous sommes 10 membres polyvalents et complémentaires qui regroupons nos compétences pour mettre au point l'application qui vise à simplifier et fiabiliser les vacations, dans les structures de la santé.</p>
       </article>
-
       <article>
-        <h2 class="h2_dimension">Pour les vacataires ...</h2>
+        <h2>Pour les vacataires ...</h2>
         <div class="register_images">
           <div class="flex_div">
-            <img class="flex_images image_logo_height" src="post_redim.png" alt="post" />
+            <img class="flex_images" src="post.png" alt="post" />
             <p class="flex_text">Inscrivez-vous</p>
           </div>
           <div class="flex_div">
@@ -47,25 +41,22 @@
           </div>
         </div>
       </article>
-
     </section>
-
     <hr class="division" />
-
     <section class="container">
       <article>
         <h2>Pour les structures ...</h2>
         <div class="register_images">
           <div class="flex_div">
-            <img class="flex_images image_logo_height" src="filter_redim.png" alt="filter" />
+            <img src="filter.png" alt="filter" />
             <p class="flex_text">Utilisez les filtres et gagnez du temps</p>
           </div>
           <div class="flex_div">
-            <img class="flex_images image_logo_height" src="presentation_redim.png" alt="presentation" />
+            <img src="presentation.png" alt="presentation" />
             <p class="flex_text">Trouvez le profil parfait pour vos besoins</p>
           </div>
           <div class="flex_div">
-            <img class="flex_images image_logo_height" src="puzzle_redim.png" alt="puzzle" />
+            <img src="puzzle.png" alt="puzzle" />
             <p class="flex_text">Rencontrez le profil</p>
           </div>
         </div>
@@ -73,37 +64,33 @@
     </section>
     <section class="valeur">
       <h2 class="valeur_titre">Nos valeurs</h2>
-      <div class="register_images_2">
-        <div class="flex_div flex_div_margin">
-          <img class="flex_images image_logo_height" src="high-quality_redim.png" alt="high-quality" />
+      <div class="register_images">
+        <div class="flex_div">
+          <img src="high-quality.png" alt="high-quality" />
           <p class="valeur_text">Utilisez les filtres et gagnez du temps</p>
         </div>
-        <div class="flex_div flex_div_margin">
-          <img class="flex_images image_logo_height" src="solidarity_redim.png" alt="solidarity" />
+        <div class="flex_div">
+          <img src="solidarity.png" alt="solidarity" />
           <p class="valeur_text">Trouvez le profil parfait pour vos besoins</p>
         </div>
-        <div class="flex_div flex_div_margin">
-          <img class="flex_images image_logo_height" src="trust_redim.png" alt="trust" />
+        <div class="flex_div">
+          <img src="trust.png" alt="trust" />
           <p class="valeur_text">Rencontrez le profil</p>
         </div>
       </div>
     </section>
     <section class="container">
-    <p class="reseaux_text">Retrouvez nous sur les réseaux :</p>
-      <div class="reseaux_sociaux">
+      <div class="register_images">
         <div>
-          <a href="#"><img class="reseaux_logo flex_images" src="facebook-2.png" alt="logo facebook" /></a>
+          <a href="#"><img src="facebook-2.png" alt="facebook-2" /></a>
         </div>
         <div>
-          <a href="#"><img class="reseaux_logo flex_images" src="linkedin-logo.png" alt="logo linkedin" /></a>
-        </div>
-        <div>
-          <a href="#"><img class="reseaux_logo flex_images" src="tiktok.png" alt="logo tiktok" /></a>
+          <a href="#"><img src="linkedin-logo.png" alt="linkedin-logo" /></a>
         </div>
       </div>
     </section>
     <div class="form">
-      <h2 class="text_form">Soyez le premier à être informé</h2>
+      <h2>Soyez le premier à être informé</h2>
       <form action="form.php" method="post">
         <div>
           <input class="email" type="mail" name="email" placeholder="Email" />
@@ -117,13 +104,10 @@
         ?>
       </form>
     </div>
-
   </main>
-
   <footer>
     <p class="footer">© 2020 Vakapp - Website developped by Ruben Barros & Théo Rossignol</p>
   </footer>
-
 </body>
 
 </html>
